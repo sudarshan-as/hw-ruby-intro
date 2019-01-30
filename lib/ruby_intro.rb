2f71e3689da4c697283944116cdd030f926dad81
sud <sudarshan.a.s96@gmail.com>
2019-01-30 13:46:23 -0600

    All tests passing

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -38,10 +38,13 @@ def sum_to_n? arr, n
 			for j in i+1 .. arr.length-1 do
 				if arr[i] + arr[j] == n
 					return true
+				else
+					check = false
 				end		#if sum is n
 			end			#for j
 		end				#for i
 	end					#if array contains more than 1 element
+	return check
 end
 
 # Part 2
@@ -55,20 +58,59 @@ def starts_with_consonant? s
   # YOUR CODE HERE
 	if s.length == 0
 		check = false
-	elsif s.match(/\A[aeiou]/i) || s.match(/\Ad/i)
-		check = false
-	else
+	elsif s.match(/\A[b-df-hj-np-tv-z]/i)
 		check = true
+	else
+		check = false
 	end
 	return check
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  # To reject empty string and non-binary numbers (not 1 or 0)
+	if s.length == 0 || s.match(/[^10]/)
+		check = false
+	else
+		if s.match(/^[10]*[00]$/) || (s==0)
+			check = true
+		else
+			check = false
+		end
+	end
+	return check
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+	# Creating a getter and setter for the variables
+	attr_accessor :isbn, :price
+
+	def initialize (isbn, price)
+		#raise ArgumentError if isbn is an empty string
+		unless isbn.length > 0
+			raise ArgumentError.new("String is empty")
+		end
+		@isbn = isbn
+
+		# raise ArgumentError if price<=0
+		unless price > 0
+			raise ArgumentError.new("Invalid price")
+		end
+		@price = price
+	end
+
+	def price_as_string
+		# contains 2 digits after decimal point
+		if (price.to_s.match(/^\d*\.\d{2}$/))		
+			price_str = "$" + price.to_s
+		# contains 1 digit after decimal point
+		elsif (price.to_s.match(/^\d*\.\d$/))		
+			price_str = "$" + price.to_s + "0"
+		# Contains no decimal point
+		else																		
+			price_str = "$" + price.to_s + ".00"
+		end
+		return price_str
+	end
 end
